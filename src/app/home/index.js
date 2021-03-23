b02886cc14a66015d4ecdb8ebbc41f65dc8a16cc
marcofuentes05 <40815025+marcofuentes05@users.noreply.github.com>
2021-03-22 18:05:47 -0600

    hotfix en hotfix (#8)

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -5,13 +5,17 @@ import { TouchableOpacity } from 'react-native-gesture-handler'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faEnvelope, faUser, faLock, faCamera } from '@fortawesome/free-solid-svg-icons'
 import { ImageBackground, StyleSheet, Dimensions, View, Text, TextInput, Image } from 'react-native'
-
+import { logout } from 'state/actions/auth'
 import { layoutColors } from 'src/settings'
 
 
-const Home = () => {
+const Home = (props) => {
+    const { clearToken } = props;
     return (
         <ImageBackground style={styles.image}>
+            <TouchableOpacity style={styles.buttonSignOut} onPress={() => clearToken()}>
+                <Text style={styles.txtSignOut}>Cerrar Sesión</Text>
+            </TouchableOpacity>
         </ImageBackground>
     )
 }
@@ -19,7 +23,11 @@ const Home = () => {
 
 export default connect(
     state => ({}),
-    dispatch => ({})
+    dispatch => ({
+        clearToken() {
+            dispatch(logout());
+        }
+    })
 )(Home)
 
 
@@ -27,5 +35,26 @@ const styles = StyleSheet.create({
     image: {
         flex: 1,
         backgroundColor: layoutColors.gray
-    }
+    },
+
+    buttonSignOut: {
+        alignItems: 'center',
+        marginTop: 33
+    },
+    txtSignOut: {
+        shadowColor: layoutColors.shadow,
+        shadowOffset: { width: 5, height: 6 },
+        shadowOpacity: 0.6,
+        elevation: 5,
+        zIndex: 5,
+        borderRadius: 22,
+        width: 250,
+        padding: 12,
+        backgroundColor: layoutColors.color1,
+
+        color: layoutColors.white,
+        fontSize: 20,
+        fontFamily: 'Poppins-Regular',
+        textAlign: 'center',
+    },
 })
