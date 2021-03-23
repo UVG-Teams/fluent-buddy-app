f314d47116a4064220453ee18341a4f8f3ccbbf4
Andy5am <38666859+Andy5am@users.noreply.github.com>
2021-03-22 21:57:41 -0600

    Sign up

---
+++ src/state/actions/signUp.js
@@ -0,0 +1,19 @@
+import * as types from 'state/types/signUp';
+
+export const startSignUp = (username, password, email) => ({
+    type: types.SIGN_UP_STARTED,
+    payload: {
+      username,
+      password,
+      email,
+    },
+  });
+
+export const completeSignUp = () => ({
+    type: types.SIGN_UP_COMPLETED,
+});
+
+export const failSignUp = error => ({
+    type: types.SIGN_UP_FAILED,
+    payload: { error },
+});
