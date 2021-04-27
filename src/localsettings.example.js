9ebf08f88b77a9c24bbb0922248838ce6e1cc8f8
FranciscoRosal <ros18676@uvg.edu.gt>
2021-04-26 19:08:01 -0600

    fix

--- src/localsettings.example.js
+++ src/localsettings.example.js
@@ -1,1 +1,11 @@
 export const LOCAL_URL = "http://192.168.1.13:3000/api"
+
+const firebaseConfigCredentials = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+}
